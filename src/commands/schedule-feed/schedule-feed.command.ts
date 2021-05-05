d2c89b1e20727a10e052a82bee104ffb0547a36d
Eduard Ilin <ei2@ukrosoft.com.ua>
2021-05-05 22:25:31 +0300

    fix: check reply_to_message is undefined

--- src/commands/schedule-feed/schedule-feed.command.ts
+++ src/commands/schedule-feed/schedule-feed.command.ts
@@ -8,7 +8,7 @@ import { Config } from "../../config";
 export class ScheduleFeedCommand extends Command {
   constructor(bot: Telegraf<Context<Update>>) {
     super(bot);
-  } 
+  }
 
   init() {
     this.bot.hears(/^[Зз]апланировать (пир|кормежку) \d\d:\d\d$/, ctx => this.handle(ctx));
@@ -28,7 +28,7 @@ export class ScheduleFeedCommand extends Command {
   }
 
   private handleAnswer(ctx: Context<Update> & {update: any}) {
-    if (ctx.update.message.reply_to_message.from.username === this.bot.botInfo?.username) {
+    if (ctx.update.message.reply_to_message?.from.username === this.bot.botInfo?.username) {
       const nextTime = moment().add(12, 'hours');
       this.botSchedule(nextTime.toDate(), ctx.update.message.message_id);
       ctx.reply('Кормежка запланирована на ' + nextTime.format('YYYY-MM-DD HH:mm'));
