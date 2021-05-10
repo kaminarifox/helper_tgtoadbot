619562034ed73a9239785826833ec0eb1a04f654
Eduard Ilin <ei2@ukrosoft.com.ua>
2021-05-10 15:23:04 +0300

    feat: automaticaly translate en layout to ru for inline queries

--- src/commands/inline/inline.command.ts
+++ src/commands/inline/inline.command.ts
@@ -1,7 +1,8 @@
-import { Context, Telegraf } from "telegraf";
-import { queryResults } from "./query-results";
-import { Command } from "../command";
-import { Update } from "typegram";
+import { Context, Telegraf } from 'telegraf';
+import { queryResults } from './query-results';
+import { Command } from '../command';
+import { Update } from 'typegram';
+import * as ru from 'convert-layout/ru';
 
 export class InlineCommand extends Command {
   constructor(bot: Telegraf<Context<Update>>) {
@@ -10,13 +11,17 @@ export class InlineCommand extends Command {
 
   init() {
     this.bot.on('inline_query', (ctx) => {
-      const query = ctx.inlineQuery.query;
+      let query = ctx.inlineQuery.query;
+
+      if (/^[a-z]+$/ui.test(query)) {
+        query = ru.fromEn(query);
+      }
 
       const filtered = queryResults.filter(v => {
         return (new RegExp(query, 'gium').test(v.title));
-      })
+      });
 
-      ctx.answerInlineQuery(filtered)
-    })
+      ctx.answerInlineQuery(filtered);
+    });
   }
 }
--- src/commands/scheduler/commands/feed-toad.ts
+++ src/commands/scheduler/commands/feed-toad.ts
@@ -1,15 +1,16 @@
-import { Agenda } from "agenda";
-import { HearContext } from "../../../types";
-import * as moment from "moment";
-import { SchedulerCommand } from "../scheduler-command";
+import { Agenda } from 'agenda';
+import { HearContext } from '../../../types';
+import * as moment from 'moment';
+import { SchedulerCommand } from '../scheduler-command';
 
 export async function feedToad(agenda: Agenda, ctx: HearContext) {
   if (ctx.update.message.reply_to_message?.from.username === ctx.botInfo?.username) {
     const addHours = /^пришло время пира$/ui.test(ctx.update.message.reply_to_message.text) ? 6 : 12;
     const nextTime = moment().add(addHours, 'hours');
 
+    // Prevent serialization errors
     const {tg, ...clearedCtx} = ctx;
-    await agenda.schedule(nextTime.toDate(), SchedulerCommand.ScheduleToadFeeding, {ctx: clearedCtx}).then()
+    await agenda.schedule(nextTime.toDate(), SchedulerCommand.ScheduleToadFeeding, {ctx: clearedCtx}).then();
 
     const replyMessage = addHours === 6 ? 'Следующий пир запланирован на ' : 'Следующая кормежка запланирована на ';
     return ctx.reply(replyMessage + nextTime.utcOffset(3).format('YYYY-MM-DD HH:mm'), {
